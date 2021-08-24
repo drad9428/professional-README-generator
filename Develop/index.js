ad1dbd7f17d2b19c78c7c42692d5119dd7cd11a7
drad <robertjdstoddard@gmail.com>
2021-08-24 12:48:37 -0600

    questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,73 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Input name:'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'GitHub username:'
+    },
+    {
+        type: 'input',
+        name: 'project',
+        message: 'Project name:'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Project description:'
+    },
+    {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'Languages used (all that apply):',
+        choices: ['JavaScript', 'HTML', 'CSS', 'Node', 'jQuery', 'Bootstrap']
+    },
+    {
+        type: 'input',
+        name: 'link',
+        message: 'GitHub project link:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation steps:'
+    },
+    {
+        type: 'input',
+        name: 'instructions',
+        message: 'Instructions for use:'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'List of contriputers:'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Type of License:',
+        choices: ['Apache 2.0', 'GPL', 'LGPL', 'MIT', 'MPL 2.0', 'BSD 3', 'BSD 2', 'CDDL 1.0', 'EPL 2.0']
+    },
+    {
+        type: 'input',
+        name: 'test',
+        messages: 'Test instructions:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Email:'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
